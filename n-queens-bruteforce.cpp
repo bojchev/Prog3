9871e0f1e60efef64d7a31c67f701bb4a24a7d5e
Nikola Bojchev <nikolabojcev2008@gmail.com>
2025-02-11 22:20:11 +0100

    Valid func

--- n-queens-bruteforce.cpp
+++ n-queens-bruteforce.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+bool Valid(vector<int>& positions, int row) {
+    for (int i = 0; i < row; i++) {
+        if (positions[i] == positions[row] || 
+            abs(positions[i] - positions[row]) == abs(i - row)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
 
 int main()
 {
